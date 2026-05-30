c4e089060da7cc35e0ba6f7410ff6b3ff7ed8929
Anna S <ts21ann@gmail.com>
2026-05-30 12:14:05 +0200

    FEAT: karma-migration

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
+import { RouterModule } from '@angular/router';
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule, AppComponent]
-}));
+    imports: [AppComponent, RouterModule],
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
