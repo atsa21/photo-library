eb3366d047b6435d0addcfe355c8830622ec26cf
Anna S <ts21ann@gmail.com>
2026-05-30 18:59:11 +0200

    FEAT: change-styles-header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { NAV_LIST } from '@core/constants';
 import { NavListModel } from '@core/models';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
