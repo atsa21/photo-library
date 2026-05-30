d919c3076db06bfe0c8ef9ba1c00fe6052a7e63f
Anna S <ts21ann@gmail.com>
2026-05-30 18:23:47 +0200

    FEAT: add-loader

--- src/app/features/photos/photos.component.spec.ts
+++ src/app/features/photos/photos.component.spec.ts
@@ -11,6 +11,13 @@ import { PhotoModel } from '@core/models';
 const MOCK_PHOTOS: PhotoModel[] = [
   { id: '0', url: 'test0.jpg', thumbUrl: 'test0.jpg' },
   { id: '1', url: 'test1.jpg', thumbUrl: 'test1.jpg' },
+  { id: '2', url: 'test2.jpg', thumbUrl: 'test2.jpg' },
+  { id: '3', url: 'test3.jpg', thumbUrl: 'test3.jpg' },
+  { id: '4', url: 'test4.jpg', thumbUrl: 'test4.jpg' },
+  { id: '5', url: 'test5.jpg', thumbUrl: 'test5.jpg' },
+  { id: '6', url: 'test6.jpg', thumbUrl: 'test6.jpg' },
+  { id: '7', url: 'test7.jpg', thumbUrl: 'test7.jpg' },
+  { id: '8', url: 'test8.jpg', thumbUrl: 'test8.jpg' },
 ];
 
 describe('PhotosComponent', () => {
@@ -39,21 +46,11 @@ describe('PhotosComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should call getPhotos onInit', () => {
-    const getPhotosSpy = spyOn(component, 'getPhotos');
+  it('should call getPhotos onInit and set photosList', () => {
+    const getPhotosSpy = spyOn(component as any, 'getPhotos');
     
     component.ngOnInit();
     expect(getPhotosSpy).toHaveBeenCalledOnceWith();
-  });
-
-  it('should update filters page and call getPhotos with page 1', () => {
-    component['getPhotos'](true);
-
-    expect(component['filters']().page).toBe(1);
-    expect(photosService.getPhotos).toHaveBeenCalledWith({ page: 1, limit: 9 });
-  });
-
-  it('should set photoList on getPhotos', () => {
     expect(component.photoList().length).toBe(MOCK_PHOTOS.length);
   });
 
@@ -62,11 +59,57 @@ describe('PhotosComponent', () => {
     expect(cards.length).toBe(MOCK_PHOTOS.length);
   });
 
-  it('should call getPhotos with isPaginated on scrolled event', () => {
-    const directiveEl = fixture.debugElement.query(By.directive(InfiniteScrollDirective));
-    directiveEl.triggerEventHandler('scrolled', null);
+  it('should show load spinner on isPaginateLoading true', () => {
+    component.isPaginateLoading.set(true);
+    const spinner = fixture.nativeElement.querySelector('app-loader');
+    expect(spinner).toBeDefined();
+  });
 
-    expect(photosService.getPhotos).toHaveBeenCalledWith({ page: 1, limit: 9 });
-    expect(component.photoList().length).toBe(18);
+  describe('paginatePhotos', () => {
+    let getPhotosSpy: jasmine.Spy;
+
+    beforeEach(() => {
+      getPhotosSpy = spyOn(component as unknown as { getPhotos: (isPaginated?: boolean) => void }, 'getPhotos');
+    });
+
+    it('should call paginatedPhotos with isPaginated on scrolled event', () => {
+      const directiveEl = fixture.debugElement.query(By.directive(InfiniteScrollDirective));
+      directiveEl.triggerEventHandler('scrolled', null);
+
+      expect(getPhotosSpy).toHaveBeenCalledOnceWith(true);
+    });
+
+    it('should set isPaginateLoading to true and call getPhotos with isPaginated', () => {
+      component.paginatePhotos();
+
+      expect(component.isPaginateLoading()).toBeTrue();
+      expect(getPhotosSpy).toHaveBeenCalledOnceWith(true);
+    });
+
+    it('should append photos and reset isPaginateLoading after pagination completes', () => {
+      getPhotosSpy.and.callThrough();
+      component.paginatePhotos();
+
+      expect(component.photoList().length).toBe(MOCK_PHOTOS.length * 2);
+      expect(component.isPaginateLoading()).toBeFalse();
+    });
+  });
+
+  describe('getPhotos', () => {
+    it('should call getPhotos with page 0', () => {
+      component['getPhotos']();
+
+      expect(component['filters']().page).toBe(0);
+      expect(photosService.getPhotos).toHaveBeenCalledWith({ page: 0, limit: 9 });
+      expect(component.photoList().length).toBe(MOCK_PHOTOS.length);
+    });
+
+    it('should update filters page and call getPhotos with page 1', () => {
+      component['getPhotos'](true);
+
+      expect(component['filters']().page).toBe(1);
+      expect(photosService.getPhotos).toHaveBeenCalledWith({ page: 1, limit: 9 });
+      expect(component.photoList().length).toBe(MOCK_PHOTOS.length * 2);
+    });
   });
 });
--- src/app/features/photos/photos.component.ts
+++ src/app/features/photos/photos.component.ts
@@ -6,16 +6,19 @@ import { PhotosService } from '@core/services/photos.service';
 import { PhotoCardComponent } from '@shared/components/photo-card/photo-card.component';
 import { take } from 'rxjs';
 import { InfiniteScrollDirective } from "@shared/directives/infinite-scroll.directive";
+import { LoaderComponent } from "@shared/components/loader/loader.component";
 
 @Component({
   selector: 'app-photos',
-  imports: [PhotoCardComponent, InfiniteScrollDirective],
+  imports: [PhotoCardComponent, InfiniteScrollDirective, LoaderComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './photos.component.html',
   styleUrl: './photos.component.scss',
 })
 export class PhotosComponent implements OnInit {
-  public photoList = signal<PhotoModel[]>([]);
+  photoList = signal<PhotoModel[]>([]);
+  isPaginateLoading = signal(false);
+
   private filters = signal<FiltersModel>(DEFAULT_FILTERS);
   private photosService = inject(PhotosService);
   private destroyRef = inject(DestroyRef);
@@ -24,7 +27,12 @@ export class PhotosComponent implements OnInit {
     this.getPhotos();
   }
 
-  getPhotos(isPaginated = false): void {
+  paginatePhotos(): void {
+    this.isPaginateLoading.set(true);
+    this.getPhotos(true);
+  }
+
+  private getPhotos(isPaginated = false): void {
     if (isPaginated) {
       this.filters.update((filter) => ({ ...filter, page: filter.page + 1 }));
     }
@@ -32,7 +40,12 @@ export class PhotosComponent implements OnInit {
       .getPhotos(this.filters())
       .pipe(take(1), takeUntilDestroyed(this.destroyRef))
       .subscribe((res) => {
-      isPaginated ? this.photoList.update((photo) => ([...photo, ...res])) : this.photoList.set(res);
+      if (isPaginated) {
+        this.photoList.update((photo) => ([...photo, ...res]))
+        this.isPaginateLoading.set(false);
+      } else {
+        this.photoList.set(res)
+      }
     })
   }
 }
