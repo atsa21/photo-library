a1a6b8cb9ad1e024c9b9f5b99fbb58b5cc547bfd
Anna S <ts21ann@gmail.com>
2026-05-30 12:17:25 +0200

    FEAT: add-basic-app-structure

---
+++ src/app/features/photo-details/photo-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-details',
+  imports: [],
+  templateUrl: './photo-details.component.html',
+  styleUrl: './photo-details.component.scss',
+})
+export class PhotoDetailsComponent {
+
+}
